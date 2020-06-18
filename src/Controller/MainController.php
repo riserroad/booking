dc41e8f676ccd344a7952f7c8d5dd7bbfe52804c
Rémi <ponche62880@gmail.com>
2020-06-18 14:40:56 +0200

    create the main controller
    the main controller is create but it will be modified

---
+++ src/Controller/MainController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="main")
+     */
+    public function index()
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+    /**
+     * @Route("/event", name="event")
+     */
+    public function event()
+    {
+        return $this->render('main/event.html.twig'); 
+    }
+}
