8a0a9e3c138d6932f4d2b0f725d905da777f688e
Rémi <ponche62880@gmail.com>
2020-06-20 23:07:32 +0200

    change the name of route main to home

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,16 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Event; 
+use App\Repository\EventRepository;
 
 class MainController extends AbstractController
 {
     /**
-     * @Route("/", name="main")
+     * @Route("/", name="home")
      */
     public function index()
     {
+
+        // recupere la liste des event 
+        $events = $this->getDoctrine()->getRepository(Event::class)->findAll(); 
+
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+            'events' => $events, 
         ]);
     }
     /**
