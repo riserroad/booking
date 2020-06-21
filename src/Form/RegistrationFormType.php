9d2cd6a1dbd9058264857408dc0839fdf29f828d
Rémi <ponche62880@gmail.com>
2020-06-21 02:08:39 +0200

    change the form to confirmation password
    demamande à l'utilisteur de comfirmer le mot de passe , pour éviter les
    fautes de frappes lors de la création du MDP

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -28,9 +29,12 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
+            ->add('plainPassword', RepeatedType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+                'type' => PasswordType::class,
+                'first_options' => ['label' => 'Password'], 
+                'second_options' => ['label' => 'Retype Password'],
                 'mapped' => false,
                 'constraints' => [
                     new NotBlank([
